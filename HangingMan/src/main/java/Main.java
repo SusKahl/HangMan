07f7fb9f46c34bd2ac3082a89125903282a6f91f
SusKahl <suskahl@gmail.com>
2015-03-24 09:24:30 +0100

    wordlist, wordpick, -outprint

--- HangingMan/src/main/java/Main.java
+++ HangingMan/src/main/java/Main.java
@@ -1,5 +1,6 @@
 
 import java.io.Console;
+import java.util.Random;
 import java.util.Scanner;
 
 /*
@@ -14,7 +15,25 @@ import java.util.Scanner;
 public class Main {
     public static String secretWord = "Hello";
     public static String name;
+    public static String[] wordList = { "computer", "java", "activity", "alaska",
+            "appearance", "javatar", "automobile", "falafel", "birthday",
+            "canada", "central", "character", "chicken", "chosen", "cutting",
+            "daily", "darkness", "shawarma", "disappear", "driving", "effort",
+            "establish", "exact", "establishment", "fifteen", "football",
+            "foreign", "frequently", "frighten", "function", "gradually",
+            "hurried", "identity", "importance", "impossible", "invented",
+            "italian", "journey", "lincoln", "london", "massage", "minerals",
+            "outer", "paint", "particles", "personal", "physical", "progress",
+            "quarter", "recognise", "replace", "rhythm", "situation",
+            "slightly", "steady", "stepped", "strike", "successful", "sudden",
+            "terrible", "traffic", "unusual", "volume", "yesterday" };
+    public static int randomNumber = 0;
+    public static String wordToGuess;
+    public static Random randomGenerator = new Random();
+    
     public static void main(String[] args) {
+        
+        
         Scanner sc = new Scanner(System.in);
         
         
@@ -26,8 +45,17 @@ public class Main {
         
         System.out.println("hello " + name);
         
+        randomNumber = randomGenerator.nextInt(wordList.length);
+        wordToGuess= wordList[randomNumber];
+        
+        for (char i : wordToGuess.toCharArray()){
+            System.out.print("_ ");
+        }
+        System.out.println();
+        
         System.out.print("Please enter the first letter :");
         String chars = sc.next();
+        
         System.out.println("You have entered  " + chars);     
     }
     
