87b1ed535eb73cc4516ed5916b60bcb24628f5a4
Christian Haase <Christian.haase.93@gmail.com>
2015-04-21 09:22:45 +0200

    Import fix

--- HangingMan/src/test/java/HangingManTest.java
+++ HangingMan/src/test/java/HangingManTest.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 
-import com.sun.xml.internal.fastinfoset.util.CharArray;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
